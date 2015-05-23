3b6928d879c604aa6f4b2dc355f396f5151f2e22
Kostas Loupasakis <loupax@gmail.com>
2015-05-23 11:12:45 +0300

    Added a basic isImage function to the URLUtils object

--- package.js
+++ package.js
@@ -13,6 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.addFiles('url-utils.js');
+  api.export('URLUtils');
 });
 
 Package.onTest(function(api) {
--- url-utils.js
+++ url-utils.js
@@ -1,1 +1,6 @@
 // Write your package code here!
+URLUtils = {
+  isImage: function URLUtilsIsImage(string){
+      return /^(https|http):\/\/.+\.(gif|png|jpg|jpeg|svg)$/i.test(string);
+  }
+};
